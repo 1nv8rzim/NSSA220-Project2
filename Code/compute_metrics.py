609c7d105b83da2c434ef036787b9a182738492d
Max <msf9542@rit.edu>
2022-04-18 15:15:14 -0400

    finished more metrics, pending testing

--- Code/compute_metrics.py
+++ Code/compute_metrics.py
@@ -9,6 +9,17 @@ def compute_metrics(capture, ip):
     reply_recv = len(capture.lambda_filter(lambda cap: cap.destination == ip and 'Echo (ping) reply' in cap.info))
     
     print(total, request, request_sent, request_recv, reply, reply_sent, reply_recv)
+    
+    request_bytes_sent = sum(packet.length for packet in capture.lambda_filter(lambda cap: cap.source == ip and 'Echo (ping) request' in cap.info).packets)
+    request_bytes_recv = sum(packet.length for packet in capture.lambda_filter(lambda cap: cap.destination == ip and 'Echo (ping) request' in cap.info).packets)
+    request_bytes_data_sent = sum(packet.length - 42 for packet in capture.lambda_filter(lambda cap: cap.source == ip and 'Echo (ping) request' in cap.info).packets)
+    request_bytes_data_recv = sum(packet.length - 42 for packet in capture.lambda_filter(lambda cap: cap.destination == ip and 'Echo (ping) request' in cap.info).packets)
+    
+    print(request_bytes_sent, request_bytes_recv, request_bytes_data_sent, request_bytes_data_recv)
+    
+    print()
+
+    
 
 def compute(node1, node2, node3, node4) :
 	compute_metrics(node1, '192.168.100.1')
